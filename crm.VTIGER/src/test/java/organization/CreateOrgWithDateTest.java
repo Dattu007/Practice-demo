78f252749504ded9096b68536c34a07d52290b13
dell <dell@DESKTOP-CTRJ7QU>
2022-03-28 14:42:21 +0530

    First commit

---
+++ crm.VTIGER/src/test/java/organization/CreateOrgWithDateTest.java
@@ -0,0 +1,18 @@
+package organization;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgWithDateTest {
+	
+	@Test
+	public void createOrgWithDate() {
+		System.out.println("created Org With Date");
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ crm.VTIGER/src/test/java/organization/CreateOrganizationTest.java
@@ -0,0 +1,56 @@
+package organization;
+
+import org.testng.annotations.Test;
+
+public class CreateOrganizationTest {
+
+	@Test
+	public void createOrg() {
+		
+		System.out.println("User 1 is created Organization");
+		
+		System.out.println("User 2 is created Organization");
+
+		 System.out.println(" User vijay is created organization");
+		 
+		 
+		 
+
+		
+		System.out.println("Maga Bega Maduve Madko Nodok Agtilla");
+		System.out.println("Maga Push Ayta");
+		
+		
+		
+		System.out.println("Maga pakka nindh adhmele agthini");
+		System.out.println("updated pull aytha nodko");
+		
+		
+		System.out.println("Doddavnu ninu first ninde");
+		System.out.println("First madko");
+		
+		System.out.println("Maga Roja goskara ond hudga hudku");
+		
+		System.out.println("edscknnes");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+	}
+}
