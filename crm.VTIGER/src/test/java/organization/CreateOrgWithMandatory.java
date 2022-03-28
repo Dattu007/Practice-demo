f88a5d38e47a01d7094202c10baddb0f0f043fc6
dell <dell@DESKTOP-CTRJ7QU>
2022-03-28 15:02:57 +0530

    Third Commit

---
+++ crm.VTIGER/src/test/java/organization/CreateOrgWithMandatory.java
@@ -0,0 +1,13 @@
+package organization;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgWithMandatory {
+	
+	@Test
+	public void createOrgWithInd() {
+		
+		System.out.println("Org created with Manadatory Field newly Added");
+	}
+
+}
